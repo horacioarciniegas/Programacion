c3d0632537bb6ed94d555670b1fe4d9edbf573aa
Horacio <zunigabravo2@gmail.com>
2025-07-02 16:57:10 -0500

    primer commit

--- Actividad4.py
+++ Actividad4.py
@@ -1,8 +1,8 @@
-var_1 = "Python es un "
-var_1 += "lenguaje de "
-var_1 += "programacion "
-var_1 += "versátil"
+# var_1 = "Python es un "
+# var_1 += "lenguaje de "
+# var_1 += "programacion "
+# var_1 += "versátil"
 
-# operacion = var_1 + var_2 + var_3 + var_4
+# # operacion = var_1 + var_2 + var_3 + var_4
 
-print(var_1)
+# print(var_1)
