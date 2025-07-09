4b6a7faa314e74dbbe10b2f51060304808bd52a7
Horacio <horacio728az@gmail.com>
2025-07-09 17:38:20 -0500

    comentario

--- Prueva_SENA_github.py
+++ Prueva_SENA_github.py
@@ -1,12 +1,99 @@
 #Hola Horacio#
-# 2. Calcula el mayor de dos numeros ingresados.
-numero1 = float(input("ingrea el primer numero: "))
-numero2 = float(input("ingrea el segundo numero: "))
+#---------------------------------------------TALLER 3-------------------------------------------------#
 
-if numero1 > numero2:
-    print("el numero mayor es:", numero1)
-elif numero2 > numero1:
-    print("el numero mayor es:", numero2)
-else:
-    print("ambos numero son iguales")
+# 1. Verifica si un numero es positivo, negativo o cero
+# numero = float (input("ingresa un numero: "))
+# if numero > 0:
+#     print("el numero es positivo.")
+# elif numero < 0:
+#     print("el numero es negativo.")
+# else:
+#     print("el numero es cero.")
+
+# 2. Calcula el mayor de dos numeros ingresados.
+# numero1 = float(input("ingresa el primer numero: "))
+# numero2 = float(input("ingresa el segundo numero: "))
+
+# if numero1 > numero2:
+#     print(f"el numero mayor es:{numero1}")
+# elif numero2 > numero1:
+#     print(f"el numero mayor es:{numero2}")
+# else:
+#     print("ambos numero son iguales")
+
+# 3. Determina se un numero es par o impar.
+# numero = int(input("ingresa un numero:"))
+# if numero % 2 == 0:
+#     print("el numero es par.")
+# else:
+#     print("el numero es impar")
+
+# 4. Dado un numero, verifica si esta entre 10 y 20.
+# numero = int(input("ingresa un numero:"))
+# if 10 <= numero <= 20:
+#     print("el numero si esta entre 10 y 20.")
+# else:
+#     print("el numero no esta entre 10 y 20")
+
+# 5. Dados tres numeros, encuentra el mayor usando condicionales.
+# num1 = float(input("ingresa el primer numero: "))
+# num2 = float(input("ingresa el segundo numero: "))
+# num3 = float(input("ingresa el tercer numero: "))
+
+# if num1 >= num2 and num1 >= num3:
+#    print(f"el numero mayor es:{num1}")
+# elif num2 >= num1 and num2 >= num3:
+#    print(f"el numero mayor es: {num2}")
+# else:
+#    print(f"el numero meyor es:{num3}")
+
+# 6. Calcula el precio final con un 10% de decuento si el total es mayor a $100.
+# total = float(input("ingresa el total de la compra: "))
+
+# if total > 100: 
+#    total *= 0.9
+#    print(f"se aplica un 10% de descuento{total}")
+# else:
+#    print(f"no se aplico descuento.{total}")
+
+# 7. Verifica si una persona puede votar(mayor o igual a 18 años).
+# edad = int(input("Ingrese la edad: "))
+
+# if edad >= 18:
+#     print("Puede votar.")
+# else:
+#     print("No puede votar.")
+# 8. Aplica un 20% de descuento solo si el cliente es VIP.
+# precio = float(input("Ingrese el precio: "))
+# tipo = input("Ingrese el tipo de cliente (VIP o normal): ")
+
+# if tipo == "VIP":
+#     precio *= 0.8
+#     print("Descuento del 20% aplicado.")
+# else:
+#     print("No se aplicó descuento.")
+
+# print("Precio final:", precio)
+
+#9. Determina si un número es múltiplo de 5 y 3 al mismo tiempo.
+
+# numero = int(input("Ingrese un número: "))
+
+# if numero % 5 == 0 and numero % 3 == 0:
+#     print("El número es múltiplo de 5 y 3.")
+# else:
+#     print("El número no es múltiplo de 5 y 3 al mismo tiempo.")
+
+# 10. Verifica si es divisible entre dos números dados.
+# numero = int(input("Ingrese el número: "))
+# div1 = int(input("Ingrese el primer divisor: "))
+# div2 = int(input("Ingrese el segundo divisor: "))
+
+# if numero % div1 == 0 and numero % div2 == 0:
+#     print(f"El número {numero} es divisible entre {div1} y {div2}.")
+# else:
+#     print(f"El número {numero} no es divisible entre ambos divisores.")
+
+
+
 
