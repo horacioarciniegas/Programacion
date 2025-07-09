a717a21a650c3674405d4dabd4da59cb0aa40da9
Horacio <horacio728az@gmail.com>
2025-07-09 16:35:42 -0500

    prueva

--- Prueva_SENA_github.py
+++ Prueva_SENA_github.py
@@ -1,1 +1,12 @@
 #Hola Horacio#
+# 2. Calcula el mayor de dos numeros ingresados.
+numero1 = float(input("ingrea el primer numero: "))
+numero2 = float(input("ingrea el segundo numero: "))
+
+if numero1 > numero2:
+    print("el numero mayor es:", numero1)
+elif numero2 > numero1:
+    print("el numero mayor es:", numero2)
+else:
+    print("ambos numero son iguales")
+
