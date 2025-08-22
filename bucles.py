da0b48cef4a4268bcb8b49037fcaeaf5cbfa5a74
Horacio <horacio728az@gmail.com>
2025-08-22 17:37:47 -0500

    Trabajo clase while

---
+++ bucles.py
@@ -0,0 +1,59 @@
+# numero = int(input("ingresa un numero: "))
+# while numero > 1:
+#     print(f"{numero}")
+#     numero -= 1
+#     print(f"finaliza el sistema")
+
+# num = int(input("ingresa un munero: "))
+
+# i = 1
+
+# print(f"\n inicia el contador en 1 {num}: ")
+
+# while i <= 10:
+#     print(f"{num} * {i} = {num * i}")
+#     i += 1
+
+# x = 5
+# while True:
+#   x -= 1
+#   print(x)
+#   if x ==0:
+#     break
+#   print("Fin del bucle")
+
+# chance = 1
+# while chance <= 3:
+#     txt =input("escribe SI: ")
+#     if txt =="SI":
+#         print("OK, lo conseguiste en el intento", chance)
+#         break
+#     chance +=1
+# else:
+#     print("has agotado tus tres intentos")
+
+# ----------- Ejercicio 1 Suma hasta cero -------------#
+# total = 0
+
+# numero = int(input("ingresa primer numero entero: "))
+
+# while numero != 0:
+#     total += numero
+#     numero = int (input("ingresa segundo numero entero: "))
+
+# print("la suma total es:", total)
+
+# ----------- Ejercicio 2 Contraseña secreta -------------#
+
+# clave = ""
+# while clave != "horacio123":
+#     clave = input("ingresa la contraseña: ")
+# print("contraseña correcta")
+
+
+
+
+
+
+
+
